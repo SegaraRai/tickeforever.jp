a1fcf52afb804b1a26dd80cd9e88750656afbcbf
SegaraRai <SegaraRai@users.noreply.github.com>
2025-01-03 05:40:53 +0900

    make og image url fully qualified

--- astro.config.ts
+++ astro.config.ts
@@ -3,8 +3,9 @@ import UnoCSS from "unocss/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  compressHTML: true,
+  site: "https://tickeforever.jp",
   trailingSlash: "never",
+  compressHTML: true,
   build: {
     format: "file",
   },
